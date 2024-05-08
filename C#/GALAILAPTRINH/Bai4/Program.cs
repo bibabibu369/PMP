2bbf9030279a8c25495bed81e323c76ffca7e8ab
bibabibu369 <fedorra.2004@gmail.com>
2024-05-08 15:15:28 +0700

    yet so

--- C#/GALAILAPTRINH/Bai4/Program.cs
+++ C#/GALAILAPTRINH/Bai4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespace {
+    public class Program {
+        static void Main(string[] args) {
+            const float Pi = 3.14f;
+            float r;
+            Console.WriteLine("Nhap ban kinh r: ");
+            r = float.Parse(Console.ReadLine()??"0");
+            Console.WriteLine("Chu vi hinh tron la: " + (2 * r * Pi));
+            Console.WriteLine("Dien tich hinh tron la: " + (r * r * Pi));
+        }
+    }
+}
