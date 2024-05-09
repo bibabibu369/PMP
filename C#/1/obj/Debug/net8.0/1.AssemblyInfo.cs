ce2afe5e620bed2785690b8954d5ed0409f8daaa
bibabibu369 <fedorra.2004@gmail.com>
2024-05-09 07:35:15 +0700

    up

--- C#/1/obj/Debug/net8.0/1.AssemblyInfo.cs
+++ C#/1/obj/Debug/net8.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5d069f391e34da2bb1b84841e42da4d0c7ab66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231522d9afcbeec56714f3e05bde8d29ea137c2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
