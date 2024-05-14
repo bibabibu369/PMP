2c98b90d3e702728734bed311383cb096e4506bc
bibabibu369 <fedorra.2004@gmail.com>
2024-05-15 00:30:18 +0700

    okay

--- C#/1/obj/Debug/net8.0/1.AssemblyInfo.cs
+++ C#/1/obj/Debug/net8.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2afe5e620bed2785690b8954d5ed0409f8daaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2096055c7025efdb32b4a08088113bce03d9a3e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Winforms/tuti/Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace tuti;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-}
